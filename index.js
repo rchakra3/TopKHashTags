b87d2f30b826e8c6cf4a5a03309435c11d961f02
Rohan Chakravarthy <rohanc219@gmail.com>
2015-03-30 22:13:30 -0400

    Updates to the hashmap mostly

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var hashMap = require('./lib/hashmap.js');
 var app=express();
 
 
-var twitconfig=require('./node_modules/twit/config.js');
+var twitconfig=require('./lib/config.js');
 var T=new Twit(twitconfig);
 
 
@@ -21,10 +21,33 @@ var server = app.listen(3000,function(){
 
 var twitter_stream=T.stream('statuses/sample');
 
+var my_linked_map=new hashMap(200);
+
+console.log(my_linked_map.max_len)
+
 twitter_stream.on('tweet',function(tweet){
 	if(tweet.entities.hashtags.length>0 && tweet.user.lang==='en')
 		tweet.entities.hashtags.forEach(function(hashtag){
-			console.log(hashtag.text)
+			//console.log(hashtag.text)
+
+			var count=my_linked_map.get(hashtag.text).value;
+
+			if(count===null){
+				//console.log('new value:'+hashtag.text);
+				my_linked_map.put(hashtag.text,1);
+			}
+			else{
+				my_linked_map.put(hashtag.text,count+1);
+			}
+
 		})
 		
 })
+
+
+var print_map_id=setInterval(function(){
+
+	for(var entry in my_linked_map.map){
+		console.log(entry+':'+my_linked_map.map[entry].value);
+	}
+},5000);
