405746937484d2ccdd03eacf1490c90b7c4f1cba
Rohan Chakravarthy <rohanc219@gmail.com>
2015-03-31 21:11:49 -0400

    Functionality working. Need to use socketio to push to client side.

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ var twitter_stream=T.stream('statuses/sample');
 
 var my_linked_map=new hashMap(200);
 
+var tweet_array=[];
+
 console.log(my_linked_map.max_len)
 
 twitter_stream.on('tweet',function(tweet){
-	if(tweet.entities.hashtags.length>0 && tweet.user.lang==='en')
-		tweet.entities.hashtags.forEach(function(hashtag){
-			//console.log(hashtag.text)
+	if(tweet.entities.hashtags.length>0 && tweet.user.lang==='en'){
+		/*tweet.entities.hashtags.forEach(function(hashtag){
 
 			var count=my_linked_map.get(hashtag.text).value;
 
@@ -40,14 +41,60 @@ twitter_stream.on('tweet',function(tweet){
 				my_linked_map.put(hashtag.text,count+1);
 			}
 
-		})
+		})*/
+
+		tweet_array.push(tweet.entities.hashtags);
+		//console.log(tweet_array);
+		if(tweet_array.length>1000){
+			tweet_array.shift();
+		}
+
+	}
 		
 })
 
 
 var print_map_id=setInterval(function(){
 
+
+	tweet_array.forEach(function(entry){
+
+		entry.forEach(function(hashtag){
+
+			var count=my_linked_map.get(hashtag.text).value;
+
+			if(count===null){
+					//console.log('new value:'+hashtag.text);
+				my_linked_map.put(hashtag.text,1);
+			}
+			else{
+				my_linked_map.put(hashtag.text,count+1);
+			}
+			
+	})
+	});
+
+	var freq_array=[];
+
 	for(var entry in my_linked_map.map){
-		console.log(entry+':'+my_linked_map.map[entry].value);
+		freq_array.push([my_linked_map.map[entry].value,entry])
+		//console.log(entry+':'+my_linked_map.map[entry].value);
 	}
+
+	freq_array.sort(function(a,b){
+		if(a[0]<b[0])
+			return 1;
+		else if(a[0]>b[0])
+			return -1
+
+		return 0;
+	})
+
+	var counter=0;
+
+	for(var i=0;i<10;i++)
+		console.log(freq_array[i]);
+	
+
+
 },5000);
