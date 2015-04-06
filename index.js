89ed849f00cf449eee8096da297d09990f46f9df
Rohan Chakravarthy <rohanc219@gmail.com>
2015-04-06 03:10:03 -0400

    Added bootstrap Added options to configure window size, k

--- index.js
+++ index.js
@@ -24,32 +24,38 @@ var server = app.listen(3000,function(){
 
 var twitter_stream=T.stream('statuses/sample');
 
-var tweet_window_size=1000;
+var tweet_window_size=100;
 
 var my_linked_map=new hashMap(tweet_window_size*3);
 
 var tweet_array=[];
 
-console.log(my_linked_map.max_len)
+var input_tto=0;
+var output_tto=0;
+
+//console.log(my_linked_map.max_len)
 
 twitter_stream.on('tweet',function(tweet){
-	if(tweet.entities.hashtags.length>0 && tweet.user.lang==='en'){
+	//console.log(tweet);
+	if(tweet.entities.hashtags.length>0 && tweet.lang==='en'){
 
 		tweet_array.push(tweet.entities.hashtags);
 
 		while(tweet_array.length>tweet_window_size){
+		
 			var hashtags=tweet_array.shift();
-			//console.log('Deleting:'+hashtags);
+			
 
 			hashtags.forEach(function(hashtag){
+
 				var count=my_linked_map.get(hashtag.text).value;
 				if(count!==null){
 					count-=1;
 					my_linked_map.put(hashtag.text,count);
-					//console.log('reduced count of:'+hashtag.text+' to '+count);
 				}
 			})
 		}
+		//console.log('Array length is now:'+tweet_array.length)
 
 	}
 		
@@ -59,7 +65,12 @@ var io= require('socket.io')(server);
 
 var time_interval=5;
 
+var k=10;
+
+
 var print_map_function=function(socket){
+
+	my_linked_map=new hashMap(tweet_window_size*3);
 
 	tweet_array.forEach(function(entry){
 
@@ -92,11 +103,9 @@ var print_map_function=function(socket){
 		return 0;
 	})
 
-	var counter=0;
-
 	var topk_obj={}
 
-	for(var i=0;i<10;i++){
+	for(var i=0;i<k;i++){
 		console.log(freq_array[i]);
 		topk_obj[i]=freq_array[i];
 	}
@@ -106,21 +115,32 @@ var print_map_function=function(socket){
 		socket.emit('Update_top_k',topk_obj)
 	}
 
-
 	console.log();
 
 }
 
-var print_interval_id=setInterval(print_map_function,time_interval*1000);
+//var print_interval_id=setInterval(print_map_function,time_interval*1000);
 
 io.on('connection',function(socket){
 
-	socket.on('Changed_Value',function(changed_val){
+	socket.on('New_Update_Freq',function(changed_val){
 		console.log('received changed_val');
 		time_interval=parseInt(changed_val);
 		clearInterval(print_interval_id);
 		print_interval_id=setInterval(function(){print_map_function(socket)},time_interval*1000);
 	});
+
+	socket.on('New_k_val',function(new_k_val){
+		k=parseInt(new_k_val);
+	})
+
+	socket.on('New_buffer_size',function(new_buffer_size){
+		tweet_window_size=parseInt(new_buffer_size)
+	})
+
+	print_interval_id=setInterval(function(){print_map_function(socket)},time_interval*1000);
+
+
 })
 
 
--- public/javascripts/client_actions.js
+++ public/javascripts/client_actions.js
@@ -1,31 +1,55 @@
 document.addEventListener('DOMContentLoaded',function(){
 
 	var socket=io();
-	var dropdown_node=document.getElementById('time_dropdown');
+	var dropdown_node=document.getElementById('dropdownMenuList');
 
-	dropdown_node.addEventListener('change',function(){
-		var time_selected=this.value;
-		console.log('Changed the value')
+	/*dropdown_node.addEventListener('click',function(){
+		var time_selected=this.text;
+		console.log('Changed the value:'+time_selected)
 		
-		socket.emit('Changed_Value',time_selected);
+		socket.emit('New_Update_Freq',time_selected);
 
+	})*/
+	$(".dropdown-menu li a").click( function() {
+	    var time_selected = $(this).text();
+	    console.log('Changed the value:'+time_selected)
+	    $("#freq_text_area").val(time_selected)
+	    //$("#freq_text_area").attr("placeholder",time_selected);
+		socket.emit('New_Update_Freq',time_selected);
+	});
+
+	$("#buffer_size_button").click(function(){
+		socket.emit('New_buffer_size', $("#buffer_size_text").val())
+	});
+
+	$("#k_button").click(function(){
+		socket.emit('New_k_val',$("#k_text").val())
 	})
 
 	socket.on('Update_top_k',function(new_tags){
 
 		var display_table=document.getElementById('topk-table-display');
 
-		while(display_table.firstChild){
-			display_table.removeChild(display_table.firstChild);
+		while(display_table.childNodes[0].lastChild && display_table.childNodes[0].lastChild.lastChild.tagName!=='TH'){
+			display_table.childNodes[0].removeChild(display_table.childNodes[0].lastChild);
 		}
 
 		for(var iter in new_tags){
 			var new_row=document.createElement('tr');
-			var new_td=document.createElement('td');
-			var text_val=document.createTextNode(new_tags[iter]);
-			new_td.appendChild(text_val);
-			new_row.appendChild(new_td);
-			display_table.appendChild(new_row);
+
+			//the hashtag column
+			var new_hasht_td=document.createElement('td');
+			var text_val=document.createTextNode(new_tags[iter][1]);
+			new_hasht_td.appendChild(text_val);
+
+			//the frequency column
+			var new_freq_td=document.createElement('td');
+			var text_val=document.createTextNode(new_tags[iter][0]);
+			new_freq_td.appendChild(text_val);
+
+			new_row.appendChild(new_hasht_td);
+			new_row.appendChild(new_freq_td);
+			display_table.childNodes[0].appendChild(new_row);
 		}
 	})
 
